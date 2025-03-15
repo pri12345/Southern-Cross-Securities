2a05014a450138ddc7cac8a524a264edb2e9e713
pri12345 <pridagia@gmail.com>
2025-03-15 09:46:18 +0300

    started with APIs

--- Execution/Broker-API.py
+++ Execution/Broker-API.py
@@ -1,1 +1,44 @@
-print('Pending: choosing a broker and getting familiar with its API and spreads')
+# API Basics
+# We will start with Primary SA's Websocket API, using the free Remarkets account for realtime market data
+#%%
+import pyRofex
+# pyrofex details --> https://github.com/matbarofex/pyRofex
+
+# %%
+
+pyRofex.initialize(
+    user="pridagia20974",
+    password="dpdgpG4$",
+    account="REM20974",
+    environment=pyRofex.Environment.REMARKET  # Change to PRIMARY if using live trading
+)
+
+
+# Define handlers
+def market_data_handler(message):
+    print("Market Data:", message)
+
+def error_handler(message):
+    print("Error:", message)
+
+def exception_handler(exception):
+    print("Exception:", exception)
+
+
+
+# Initialize WebSocket
+pyRofex.init_websocket_connection(
+    market_data_handler=market_data_handler,
+    error_handler=error_handler,
+    exception_handler=exception_handler
+)
+
+# Subscribe to bid/ask data
+pyRofex.market_data_subscription(
+    tickers=["MERV - XMEV - GGALD - CI"],  # Replace with desired instruments
+    entries=[pyRofex.MarketDataEntry.BIDS, pyRofex.MarketDataEntry.OFFERS, pyRofex.MarketDataEntry.LAST]
+)
+
+
+# %%
+# Seccion Datos Historicos, consultar documentacion API, esta ahi
